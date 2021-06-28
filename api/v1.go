263f874ab3bc9991a9307a39ffb708c0652f7796
vexilology <andreyisback@yandex.ru>
2021-06-28 04:08:23 +0400

    new v1

--- api/v1.go
+++ api/v1.go
@@ -1,6 +1,17 @@
 // Shikimori API 1.0
 package api
 
+// Global section
+const (
+  Search = "search"
+)
+
+// Achievements section
+const (
+  Achievements = "achievements"
+)
+
+// Animes section
 const (
   Animes         = "animes"
   Roles          = "roles"
@@ -9,4 +20,24 @@ const (
   Franchise      = "franchise"
   External_links = "external_links"
   Topics         = "topics"
+)
+
+// Appear section
+const (
+  Appears = "appears"
+)
+
+// Bans section
+const (
+  Bans = "bans"
+)
+
+// Calendar section
+const (
+  Calendar = "calendar"
+)
+
+// Characters section
+const (
+  Characters = "characters"
 )
