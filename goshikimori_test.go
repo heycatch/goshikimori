fba2d6f58a453028ffdb3bf09c575b5d298d5e18
vexilology <andreyisback@yandex.ru>
2021-09-04 22:39:03 +0400

    update test

--- goshikimori_test.go
+++ goshikimori_test.go
@@ -3,6 +3,9 @@ package goshikimori
 import (
   "log"
   "testing"
+  "math/rand"
+  "time"
+  "fmt"
 
   "github.com/vexilology/goshikimori/api"
 )
@@ -13,18 +16,25 @@ const (
   test_access_token = ""
 )
 
+func random_number(min, max int) string {
+  r := rand.Intn(max-min) + min
+  return fmt.Sprintf("%v", r)
+}
+
 func TestRequest(t *testing.T) {
+  rand.Seed(time.Now().UnixNano())
+
   req, _ := NewRequest(
     test_app,
     test_access_token,
     test_method,
-    Parameters(api.Users, api.Whoami),
+    Parameters(api.Characters, api.FoundID(random_number(1, 5))),
   )
 
-  if test_method != "" && test_app != "" && test_access_token != "" {
+  if test_app != "" && test_access_token != "" {
     t.Log("Correct reuqest")
     log.Println(string(req))
   } else {
-    t.Error("Bad method or app or token")
+    t.Error("Bad request or problems with your app")
   }
 }
