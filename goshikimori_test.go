1e6f089f47614e3ffa3860d169fdd004ec99e034
vexilology <andreyisback@yandex.ru>
2021-10-27 19:18:55 +0400

    from %v to %d

--- goshikimori_test.go
+++ goshikimori_test.go
@@ -25,9 +25,9 @@ func TestUser(t *testing.T) {
   }
   if api_test != "" && key_test != "" {
     if r.Id == 181833 && r.Sex == "male" {
-      t.Logf("User %s id %v - found", r.Nickname, r.Id)
+      t.Logf("User %s id %d - found", r.Nickname, r.Id)
     } else {
-      t.Errorf("User %s id %v - not found", r.Nickname, r.Id)
+      t.Errorf("User %s id %d - not found", r.Nickname, r.Id)
     }
   } else {
     t.Error("Not found Application or SecretKey")
@@ -43,9 +43,9 @@ func TestAnimes(t *testing.T) {
   for _, values := range r {
     if api_test != "" && key_test != "" {
       if values.Id == 12725 && values.Status == "released" {
-        t.Logf("Anime %s id %v - found", values.Name, values.Id)
+        t.Logf("Anime %s id %d - found", values.Name, values.Id)
       } else {
-        t.Errorf("Anime %s id %v - not found", values.Name, values.Id)
+        t.Errorf("Anime %s id %d - not found", values.Name, values.Id)
       }
     } else {
       t.Error("Not found Application or SecretKey")
@@ -62,9 +62,9 @@ func TestMangas(t *testing.T) {
   for _, values := range r {
     if api_test != "" && key_test != "" {
       if values.Volumes == 48 && values.Chapters == 724 {
-        t.Logf("Manga %s id %v - found", values.Name, values.Id)
+        t.Logf("Manga %s id %d - found", values.Name, values.Id)
       } else {
-        t.Errorf("Manga %s id %v - not found", values.Name, values.Id)
+        t.Errorf("Manga %s id %d - not found", values.Name, values.Id)
       }
     } else {
       t.Error("Not found Application or SecretKey")
